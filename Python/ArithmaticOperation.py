24edb5fc2fad26887c803675f9cca590640c4c05
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-09-16 13:29:19 +0000

    unit 4 python

---
+++ Python/ArithmaticOperation.py
@@ -0,0 +1,30 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Addition
+add = num1 + num2
+print("Addition: ", num1, " + ", num2, " = ", add)
+
+# Subtraction
+sub = num1 - num2
+print("Subtraction: ", num1, " - ", num2, " = ", sub)
+
+# Multiplication
+mul = num1 * num2
+print("Multiplication: ", num1, " * ", num2, " = ", mul)
+
+# Division
+div = num1 / num2
+print("Division: ", num1, " / ", num2, " = ", div)
+
+# Floor Division
+floor_div = num1 // num2
+print("Floor Division: ", num1, " // ", num2, " = ", floor_div)
+
+# Modulo (Remainder)
+mod = num1 % num2
+print("Modulo: ", num1, " % ", num2, " = ", mod)
+
+# Exponentiation
+exp = num1**num2
+print("Exponentiation: ", num1, " ** ", num2, " = ", exp)
---
+++ Python/RelationalOperations.py
@@ -0,0 +1,20 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Greater than
+print("Greater than: ", num1, " > ", num2, " = ", num1 > num2)
+
+# Less than
+print("Less than: ", num1, " < ", num2, " = ", num1 < num2)
+
+# Equal to
+print("Equal to: ", num1, " == ", num2, " = ", num1 == num2)
+
+# Not equal to
+print("Not equal to: ", num1, " != ", num2, " = ", num1 != num2)
+
+# Greater than or equal to
+print("Greater than or equal to: ", num1, " >= ", num2, " = ", num1 >= num2)
+
+# Less than or equal to
+print("Less than or equal to: ", num1, " <= ", num2, " = ", num1 <= num2)
