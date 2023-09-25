376c1419472caeafb992cfa1d7ef52ed97c19714
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-09-25 10:35:11 +0000

    program 1

--- Big-Data/maxOfThreeusingCondition.java
+++ Big-Data/maxOfThreeusingCondition.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class maxOfThreeusingCondition {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the first number: ");
+        int num1 = input.nextInt();
+        
+        System.out.print("Enter the second number: ");
+        int num2 = input.nextInt();
+        
+        System.out.print("Enter the third number: ");
+        int num3 = input.nextInt();
+        
+        int max = (num1 > num2 && num1 > num3) ? num1 : (num2 > num3 ? num2 : num3);
+        
+        System.out.println("The maximum of the three numbers is: " + max);
+    }
+}
