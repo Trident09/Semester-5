0339f29becdefc70a334863fd837063903f4cde2
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-09-26 13:49:23 +0000

    sec largest number

--- Big-Data/secMaxNowithoutArray.java
+++ Big-Data/secMaxNowithoutArray.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
 
+public class secMaxNowithoutArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the total number of numbers: ");
+        int n = scanner.nextInt();
+
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        System.out.println("Enter the numbers: ");
+        for (int i = 0; i < n; i++) {
+            int number = scanner.nextInt();
+
+            if (number > max) {
+                secondMax = max;
+                max = number;
+            } else if (number > secondMax && number < max) {
+                secondMax = number;
+            }
+        }
+
+        if (secondMax == Integer.MIN_VALUE) {
+            System.out.println("There is no second largest number.");
+        } else {
+            System.out.println("The second largest number is: " + secondMax);
+        }
+    }
+}
