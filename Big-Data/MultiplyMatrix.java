a0ef85e8b8cbf29698c7e011fdd8e5b98ac36a67
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-09-26 13:36:34 +0000

    Matrix multiplication

--- Big-Data/MultiplyMatrix.java
+++ Big-Data/MultiplyMatrix.java
@@ -1,1 +1,52 @@
+import java.util.Scanner;
 
+public class MultiplyMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows and columns in the first matrix: ");
+        int rows1 = scanner.nextInt();
+        int columns1 = scanner.nextInt();
+        System.out.print("Enter the number of rows and columns in the second matrix: ");
+        int rows2 = scanner.nextInt();
+        int columns2 = scanner.nextInt();
+
+        if (columns1 != rows2) {
+            System.out.println("Matrix multiplication is not possible. The number of columns in the first matrix must be equal to the number of rows in the second matrix.");
+            return;
+        }
+
+        int[][] matrix1 = new int[rows1][columns1];
+        int[][] matrix2 = new int[rows2][columns2];
+
+        System.out.println("Enter the elements of the first matrix: ");
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < columns1; j++) {
+                matrix1[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Enter the elements of the second matrix: ");
+        for (int i = 0; i < rows2; i++) {
+            for (int j = 0; j < columns2; j++) {
+                matrix2[i][j] = scanner.nextInt();
+            }
+        }
+
+        int[][] resultMatrix = new int[rows1][columns2];
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < columns2; j++) {
+                for (int k = 0; k < columns1; k++) {
+                    resultMatrix[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        System.out.println("The product of the matrices is: ");
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < columns2; j++) {
+                System.out.print(resultMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
