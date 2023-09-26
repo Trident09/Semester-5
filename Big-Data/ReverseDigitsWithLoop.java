060a3332c0ba37909c34ef6ceb81311916510470
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-09-26 13:46:21 +0000

    Loop reverse

--- Big-Data/ReverseDigitsWithLoop.java
+++ Big-Data/ReverseDigitsWithLoop.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class ReverseDigitsWithLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int reversedNumber = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        System.out.println("The reversed number is: " + reversedNumber);
+    }
+}
