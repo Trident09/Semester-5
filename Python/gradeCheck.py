bb03dcf929c74e00e97778fd9055e0e780698727
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-12-05 16:41:09 +0530

    changes for sgpa and multiple subjects

--- Python/gradeCheck.py
+++ Python/gradeCheck.py
@@ -1,19 +1,33 @@
-def find_grade(marks):
-   if marks == 100:
-       return "Excellent"
-   elif marks >= 90:
-       return "A"
-   elif marks >= 80:
-       return "B"
-   elif marks >= 70:
-       return "C"
-   elif marks >= 60:
-       return "D"
-   elif marks >= 50:
-       return "F"
-   else:
-       return "IF"
+def calculate_grade(marks):
+   grades = {
+       100: "Excellent",
+       90: "A",
+       80: "B",
+       70: "C",
+       60: "D",
+       50: "F",
+   }
+   for mark, grade in sorted(grades.items(), reverse=True):
+       if marks >= mark:
+           return grade
+   return "IF"
 
-marks = float(input("Enter the marks for the subject: "))
-grade = find_grade(marks)
-print("Marks: " + str(marks) + ", Grade: " + grade)
+def calculate_sgpa(n, marks):
+   grades = [0] * n
+   for i in range(n):
+       grades[i] = calculate_grade(marks[i])
+
+   sgpa = sum(marks) / n
+
+   return sgpa
+
+n = int(input("Enter the number of subjects: "))
+
+marks = []
+
+for i in range(n):
+   marks.append(int(input(f"Enter the marks for subject {i+1}: ")))
+sgpa = calculate_sgpa(n, marks)
+for i in range(n):
+   print(f"Subject {i+1}: Grade = {calculate_grade(marks[i])}, Marks = {marks[i]}")
+print("SGPA = ", '%.1f' % sgpa)
