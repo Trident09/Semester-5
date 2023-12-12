117ab7fbd2bae75089cb8da899425b28fa88f203
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-12-12 11:27:15 +0000

    minor change for differentiating the outputs

--- Python/Project_file/loops.py
+++ Python/Project_file/loops.py
@@ -1,14 +1,17 @@
 # Using a for loop
+print("Using a for loop")
 for i in range(10):
    print(i)
 
 # Using a while loop
+print("Using a while loop")
 i = 0
 while i < 10:
    print(i)
    i += 1
 
 # Using nested loops
+print("Using nested loops")
 for i in range(1, 6):
    for j in range(1, i+1):
        print(j, end=" ")
--- Python/Project_file/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-my_array = arr.array('i', [1, 2, 3, 4, 5])
-# count = my_array.count(2)
-# print("\nNumber of occurrences of 2:", count)
