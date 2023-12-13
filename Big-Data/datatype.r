166d3f6e9a5f19048ec0c39826651c00eb5f6492
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-12-13 09:30:33 +0530

    datatype in r

---
+++ Big-Data/datatype.r
@@ -0,0 +1,19 @@
+# Numeric Data Type
+numeric_value <- 3.14
+print(class(numeric_value)) # Output: "numeric"
+
+# Integer Data Type
+integer_value <- 10L
+print(class(integer_value)) # Output: "integer"
+
+# Logical Data Type
+logical_value <- TRUE
+print(class(logical_value)) # Output: "logical"
+
+# Complex Data Type
+complex_value <- 3 + 2i
+print(class(complex_value)) # Output: "complex"
+
+# Character Data Type
+character_value <- "Hello, World!"
+print(class(character_value)) # Output: "character"
